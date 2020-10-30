a6b76c295fbd1ca78493a8001d5253519686b2d7
Martin <mxz0801@users.noreply.github.com>
2020-10-29 20:08:31 -0700

    setting data field

--- starlink/src/components/SatSetting.js
+++ starlink/src/components/SatSetting.js
@@ -7,8 +7,9 @@ import {
     InputNumber
 } from 'antd';
 
-class SatSetting extends Component {
+class SatSettingForm extends Component {
     render() {
+        const {getFieldDecorator } = this.props.form;
         const formItemLayout = {
             labelCol: {
                 xs: { span: 24 },
@@ -20,17 +21,51 @@ class SatSetting extends Component {
             },
         };
         return (
-            <Form {...formItemLayout}>
+            <Form {...formItemLayout} className="sat-setting" onSubmit={this.showSatellite}>
                 <Form.Item label="Longitude(degrees)">
-                    {(<InputNumber min={-180} max={180} placeholder="Please enter a longitude" />)}
+                    {getFieldDecorator('longitude',{
+                        rules: [{required: true, message: 'Please input your longitude!'}]
+                    })(<InputNumber min={-180} max={180} placeholder="Please enter a longitude" style={{ width: "100%" }}/>)}
                 </Form.Item>
 
                 <Form.Item label="Latitude(degrees)">
-                    {(<InputNumber min={-180} max={180} placeholder="Please enter a Latitude" />)}
+                    {(<InputNumber min={-180} max={180} placeholder="Please enter a Latitude" style={{ width: "100%" }}/>)}
+                </Form.Item>
+
+                <Form.Item label="Elevation(meters)">
+                    {(<InputNumber min={-413} max={8850} placeholder="Please input your Elevation" style={{ width: "100%" }}/>)}
+                </Form.Item>
+
+                <Form.Item label="Altitude(degrees)">
+                    {(<InputNumber min={0} max={90} placeholder="Please input your Altitude" style={{ width: "100%" }}/>)}
+                </Form.Item>
+
+                <Form.Item label="Duration(secs)">
+                    {(<InputNumber min={0} max={90} placeholder="Please input Duration" style={{ width: "100%" }}/>)}
+                </Form.Item>
+
+                <Form.Item className="show-nearby">
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        style={{ textAlign: "center" }}>Find Nearby Satellite</Button>
                 </Form.Item>
             </Form>
         );
     }
+
+    showSatellite = e =>{
+        //step 1: collect data from the form
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                //console.log('Received values of form: ', values);
+                this.props.onShow(values);
+            }
+        });
+    }
 }
+
+const SatSetting = Form.create({ name: "satellite-setting" })(SatSettingForm);
 
 export default SatSetting;
