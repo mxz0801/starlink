532e836642befbda14ded0b4e2e4b046d117021a
Martin <mxz0801@users.noreply.github.com>
2020-11-07 16:37:27 -0800

    add sat map

--- starlink/src/constants.js
+++ starlink/src/constants.js
@@ -3,3 +3,5 @@ export const SAT_API_KEY = "LXPS3V-FZ9XVZ-2XNAXD-4L06";
 export const STARLINK_CATEGORY = "52";
 
 export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
